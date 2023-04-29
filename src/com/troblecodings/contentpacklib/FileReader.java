ba38c79c914586f31609d4b5c76e2b4228de0dc4
Cedric <cedric.hathaway05@gmail.com>
2023-04-29 13:48:13 +0200

    fix last compiler issues

--- src/com/troblecodings/contentpacklib/FileReader.java
+++ src/com/troblecodings/contentpacklib/FileReader.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,8 +45,9 @@ public class FileReader {
             Files.list(contentDirectory).filter(path -> path.toString().endsWith(".zip"))
                     .forEach(path -> {
                         try {
-                            paths.add(FileSystems.newFileSystem(path).getRootDirectories()
-                                    .iterator().next());
+                            paths.add(
+                                    FileSystems.newFileSystem(path.toUri(), Collections.emptyMap())
+                                            .getRootDirectories().iterator().next());
                         } catch (final IOException e) {
                             logger.error(String.format("Could not load %s!", path.toString()), e);
                         }
