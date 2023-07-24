b03fd23cbf7e4fb67051c7db936f9ebfdb995a5f
Cedric <cedric.hathaway05@gmail.com>
2023-07-24 16:49:59 +0200

    feat: more security

---
+++ src/com/troblecodings/contentpacklib/ContentPackHandler.java
@@ -0,0 +1,175 @@
+package com.troblecodings.contentpacklib;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.apache.logging.log4j.Logger;
+
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.FileResourcePack;
+import net.minecraft.client.resources.IReloadableResourceManager;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.client.resources.SimpleReloadableResourceManager;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ContentPackHandler {
+
+    @SuppressWarnings("unused")
+    private final String modid;
+    private final String internalBaseFolder;
+    private final Logger logger;
+    private final Function<String, Path> function;
+    private final Gson gson;
+    private final Path contentDirectory;
+    private final List<Path> paths = new ArrayList<>();
+    private final long hash;
+
+    public ContentPackHandler(final String modid, final String internalBaseFolder,
+            final Logger logger, final Function<String, Path> function) {
+        this.modid = modid;
+        this.internalBaseFolder = internalBaseFolder;
+        this.logger = logger;
+        this.function = function;
+        this.gson = new Gson();
+        this.contentDirectory = Paths.get("./contentpacks", modid);
+
+        if (FMLCommonHandler.instance().getSide().isClient()) {
+            ((IReloadableResourceManager) Minecraft.getMinecraft().getResourceManager())
+                    .registerReloadListener(r -> this.onResourceManagerReload(r));
+        }
+
+        try {
+            Files.createDirectories(contentDirectory);
+            Files.list(contentDirectory).filter(path -> path.toString().endsWith(".zip"))
+                    .forEach(path -> {
+                        try {
+                            paths.add(FileSystems
+                                    .newFileSystem(path, ClassLoader.getSystemClassLoader())
+                                    .getRootDirectories().iterator().next());
+
+                        } catch (final IOException e) {
+                            logger.error(String.format("Could not load %s!", path.toString()), e);
+                        }
+                    });
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+        Collections.sort(paths, (path1, path2) -> path1.compareTo(path2));
+        final AtomicLong counter = new AtomicLong(0);
+        try {
+            Files.list(contentDirectory).filter(path -> path.toString().endsWith(".zip"))
+                    .forEach(path -> {
+                        try {
+                            final ZipInputStream stream = new ZipInputStream(
+                                    new FileInputStream(path.toFile()));
+                            for (ZipEntry entry = stream
+                                    .getNextEntry(); entry != null; entry = stream.getNextEntry()) {
+                                final ZipEntry currentEntry = entry;
+                                counter.getAndUpdate(current -> current ^ currentEntry.getCrc());
+                            }
+                        } catch (final IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        hash = counter.get();
+        new NetworkContentPackHandler(modid, this);
+    }
+
+    public long getHash() {
+        return hash;
+    }
+
+    public List<Path> getPaths() {
+        return this.paths;
+    }
+
+    private String fromInternal(final String internal) {
+        return internalBaseFolder + "/" + internal;
+    }
+
+    public List<Entry<String, String>> getFiles(final String internal, final List<Path> paths) {
+        final List<Path> internalPaths = new ArrayList<>();
+        internalPaths.add(function.apply(fromInternal(internal)));
+        internalPaths.addAll(paths);
+        return getFiles(internalPaths);
+    }
+
+    public List<Entry<String, String>> getFiles(final List<Path> paths) {
+        final List<Entry<String, String>> files = new ArrayList<>();
+        paths.forEach(path -> {
+            try {
+                if (!(Files.exists(path) && Files.isDirectory(path)))
+                    return;
+                final Stream<Path> inputs = Files.list(path);
+                inputs.forEach(file -> {
+                    try {
+                        final String content = new String(Files.readAllBytes(file));
+                        final String name = file.getFileName().toString();
+                        files.add(Maps.immutableEntry(name, content));
+                    } catch (final IOException e) {
+                        logger.warn("There was a problem during reading " + file + " !");
+                        e.printStackTrace();
+                    }
+                });
+                inputs.close();
+            } catch (final IOException e) {
+                logger.warn("There was a problem during listing all files from " + path + " !");
+                e.printStackTrace();
+            }
+        });
+        return files;
+    }
+
+    public List<Entry<String, String>> getFiles(final String internal) {
+        final ArrayList<Path> externalPaths = new ArrayList<>();
+        final String fullPath = fromInternal(internal);
+        paths.forEach(path -> externalPaths.add(path.resolve(fullPath)));
+        return getFiles(internal, externalPaths);
+    }
+
+    public <T> Map<String, T> toJson(final Class<T> clazz, final Map<String, String> file) {
+        final Map<String, T> map = new HashMap<>();
+        file.forEach((name, content) -> {
+            map.put(name, gson.fromJson(content, clazz));
+        });
+        return map;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void onResourceManagerReload(IResourceManager resourceManager) {
+        if (resourceManager instanceof SimpleReloadableResourceManager) {
+            final SimpleReloadableResourceManager manager = (SimpleReloadableResourceManager) resourceManager;
+
+            try {
+                Files.list(contentDirectory).forEach(
+                        path -> manager.reloadResourcePack(new FileResourcePack(path.toFile())));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/com/troblecodings/contentpacklib/NetworkContentPackHandler.java
@@ -0,0 +1,62 @@
+package com.troblecodings.contentpacklib;
+
+import java.nio.ByteBuffer;
+
+import io.netty.buffer.Unpooled;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.play.client.CPacketCustomPayload;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraftforge.fml.common.network.FMLEventChannel;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent.ServerCustomPacketEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.internal.FMLProxyPacket;
+
+public class NetworkContentPackHandler {
+
+    private final FMLEventChannel channel;
+    private final String channelName;
+    private final ContentPackHandler handler;
+
+    public NetworkContentPackHandler(final String modid, final ContentPackHandler handler) {
+        this.channelName = new ResourceLocation(modid, "contentpackhandler").toString();
+        channel = NetworkRegistry.INSTANCE.newEventDrivenChannel(channelName);
+        this.handler = handler;
+        channel.register(this);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void serverEvent(final ServerCustomPacketEvent event) {
+        final ByteBuffer buffer = event.getPacket().payload().nioBuffer();
+        final long serverHash = buffer.getLong();
+        if (serverHash != handler.getHash()) {
+            throw new IllegalArgumentException("Server and Client Hash are not equal!"
+                    + " Please check that you have got the same ContentPacks on Client and Server!"
+                    + " Server Hash: [" + serverHash + "], Client Hash: [" + handler.getHash()
+                    + "]");
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerJoin(final PlayerLoggedInEvent event) {
+        final ByteBuffer buffer = ByteBuffer.allocate(8);
+        buffer.putLong(handler.getHash());
+        sendTo(event.player, buffer);
+    }
+
+    private void sendTo(final EntityPlayer player, final ByteBuffer buf) {
+        final PacketBuffer buffer = new PacketBuffer(
+                Unpooled.copiedBuffer((ByteBuffer) buf.position(0)));
+        if (player instanceof EntityPlayerMP) {
+            final EntityPlayerMP server = (EntityPlayerMP) player;
+            channel.sendTo(new FMLProxyPacket(buffer, channelName), server);
+        } else {
+            channel.sendToServer(new FMLProxyPacket(new CPacketCustomPayload(channelName, buffer)));
+        }
+    }
+}
