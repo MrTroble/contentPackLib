8eadc15c1df949b04e0d6ad551129066caa60c84
Cedric <cedric.hathaway05@gmail.com>
2024-04-30 15:41:09 +0200

    fix: ContentPackNet

--- src/com/troblecodings/contentpacklib/NetworkContentPackHandler.java
+++ src/com/troblecodings/contentpacklib/NetworkContentPackHandler.java
@@ -2,16 +2,19 @@ package com.troblecodings.contentpacklib;
 
 import java.nio.ByteBuffer;
 
+import org.apache.logging.log4j.LogManager;
+
 import io.netty.buffer.Unpooled;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.client.CPacketCustomPayload;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.fml.common.network.FMLEventChannel;
-import net.minecraftforge.fml.common.network.FMLNetworkEvent.ServerCustomPacketEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientCustomPacketEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.internal.FMLProxyPacket;
 
@@ -20,9 +23,11 @@ public class NetworkContentPackHandler {
     private final FMLEventChannel channel;
     private final String channelName;
     private final ContentPackHandler handler;
+    private final String modid;
 
     public NetworkContentPackHandler(final String modid, final ContentPackHandler handler) {
         this.channelName = modid + ":CPNet";
+        this.modid = modid;
         channel = NetworkRegistry.INSTANCE.newEventDrivenChannel(channelName);
         this.handler = handler;
         channel.register(this);
@@ -30,14 +35,15 @@ public class NetworkContentPackHandler {
     }
 
     @SubscribeEvent
-    public void serverEvent(final ServerCustomPacketEvent event) {
+    public void serverEvent(final ClientCustomPacketEvent event) {
         final ByteBuffer buffer = event.getPacket().payload().nioBuffer();
         final long serverHash = buffer.getLong();
         if (serverHash != handler.getHash()) {
-            throw new IllegalArgumentException("Server and Client Hash are not equal!"
+            LogManager.getLogger(modid).error("Server and Client Hash are not equal!"
                     + " Please check that you have got the same ContentPacks on Client and Server!"
                     + " Server Hash: [" + serverHash + "], Client Hash: [" + handler.getHash()
                     + "]");
+            FMLCommonHandler.instance().exitJava(0, true);
         }
     }
 
